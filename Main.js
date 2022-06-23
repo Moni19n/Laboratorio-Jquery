7b32d8f0b9222e6cdac03b2c93266b82586a138b
Moni19n <moni19n@outlook.com>
2022-06-22 20:40:13 -0600

    N

---
+++ Main.js
@@ -0,0 +1,30 @@
+
+
+
+
+
+
+//abreviatura de document.getElementById()
+
+//$('p').$
+
+
+//Selector
+//document.getElementById("p")
+
+//Función Onclick
+//function ver() {
+
+//    alert("hola")
+//}
+
+
+//$('#párrafo')
+
+//$('p').click(function (e) {
+  //  e.preventDefault();
+    //alert("hola")
+
+// });
+
+
